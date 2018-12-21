7795cfa5ea915768e6d20207531859bba5df2e74
ShohamRoditi <shohamroditi@gmail.com>
2018-12-21 21:02:44 +0200

    add validations

--- controller.js
+++ controller.js
@@ -55,7 +55,7 @@ module.exports = {
 
 
     },
-    getAssociationByYear:function(req, res){
+    getAssociationByYear:function(conditions,update, res){
         // const { year = null} = req.params
         
         // sportassociation.findOne(conditions, (err, association)=> {
@@ -65,38 +65,59 @@ module.exports = {
         //          res.status(404).send(`not found: ${err}`);
         //     }
         // })
-            const conditions = {year:req.body.year};
-            const update = {association_manager:req.body.association_manager};
+            // const conditions = {year:req.body.year};
+            // const update = {association_manager:req.body.association_manager};
             console.log(conditions)
             console.log(update)
             const opts = {multi:true};
+            // if (conditions < 0 ) {
+            //     console.log(`the year is not valid ${conditions}`);
+            //     res.send(`the year is not valid ${conditions}`);
+
+            // }
             sportassociation.updateOne(conditions, update, opts,
-                (err) => {
+                (err, result) => {
                     if(err){
                         console.log(`err: ${err}`);
                         res.send(err);
                     }
+
+                    else if(result.nModified  <= 0){
+                        res.send(`Did not update: ${JSON.stringify(update)} with conditions: ${JSON.stringify(conditions)}`);
+                    
+                    }
+                    
+                    // res.send(`Successfully updated ${JSON.stringify(update)} where conditions: ${JSON.stringify(conditions)}`);
                     else{
-                        console.log(`Successfully Updated SpoartAssocistion with year:${conditions.year} 
-                        .year is set to: ${update.association_manager}` );
-                        res.send(`Successfully Updated SpoartAssocistion with year:${conditions.year} 
-                        .year is set to: ${update.association_manager}` );
+                        console.log(`Successfully Updated SpoartAssocistion with year: ${conditions.year} .year is set to: ${update.association_manager}` );
+                        res.send(`Successfully Updated SpoartAssocistion with year: ${conditions.year} .year is set to: ${update.association_manager}` );
                     }
                 })
 
 
 
     },
-        
-    athlethes_paid_money:function(conditions, res){
-        // const number_of_athletes = {number_of_athletes:req.body.number_of_athletes};
-        // const money = { money_paid:req.body.money_paid}
+    
+    // query number 3 in GET: receives two params : organization_name and number_of_athletes
+    athlethes_of_organization:function(conditions, res){
         sportassociation.find(conditions, (err, association) => {
-            if(association) res.send(association);
-            else{
-                 console.log(`Query Error: ${err}`);
-                 res.status(404).send(`not found: ${err}`);
+            if(err){
+                console.log(`Query Error: ${err}`);
+                res.status(404).send(`not found: ${err}`);
+
             }
+            else if(association.length == 0){
+                        res.send(`No documents found: ${JSON.stringify(conditions)}`)
+                        console.log(`No documents found:  ${JSON.stringify(conditions)}`)
+                    }
+            else  {res.send(association);
+                console.log(`The query with params : ${JSON.stringify(conditions)} is success`)}
+            // else{
+            //      console.log(`Query Error: ${err}`);
+            //      res.status(404).send(`not found: ${err}`);
+            // }
+
+                        
         })
     }
   
--- index.js
+++ index.js
@@ -6,30 +6,72 @@ const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//  app.get('/associations', ctrl.getAllAssociations);
 
 app.all('*', (req, res, next) => {
     console.log("A Client is trying to log in .");
     req.next();
 })
 
-app.get('/associations', (req,res) =>{
+app.get('/associations', (req,res) => {
     ctrl.getAllAssociations(res);
     console.log("Client handled at /getAllAthletes, GET.");
 })
     
 
-app.post('/associationsyear', (req, res)=>{
-    ctrl.getAssociationByYear(req, res);
-    console.log("Client handled at /associationsyear, POST.");
+app.post('/getAssociationByYear', (req, res)=> {
+    const conditions = {year:req.body.year};
+    const update = {association_manager:req.body.association_manager};
+ 
+    if(!conditions.year || !update.association_manager){
+        console.log(`Inappropriate parameters - you need to send in body number and string. year: ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
+        res.send(`Inappropriate parameters - you need to send in body number and string. year:
+        ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
+        return;
+    }
+
+    if(conditions.year <= 0 || update.association_manager <= 0){
+        console.log(`Inappropriate parameters - you need to send in body a POSITIVE number and string . year: ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
+        res.send(`Inappropriate parameters - you need to send in body a POSITIVE number and string
+        year: ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
+        return;
+    }
+    ctrl.getAssociationByYear(conditions,update, res);
+    console.log("Client handled at /getAssociationByYear, POST.");
 
 })
 
-app.get('/athlethes_paid_money',(req, res) => {
-    const conditions = { organization_name:req.query.organization_name};
-    console.log(conditions)
-    ctrl.athlethes_paid_money(conditions,res)
-    console.log("Client handled at /athlethes_paid_money, GET.");
+app.get('/athlethes_of_organization',(req, res) => {
+    const conditions = { organization_name:req.query.organization_name, number_of_athletes:parseInt(req.query.number_of_athletes)};
+    
+    // this if check if the params that sent in body == undefined || == empty || == 0 undifiend /null/ empty/!=0
+    if(!conditions.number_of_athletes || !conditions.organization_name){
+        console.log(`Inappropriate parameters - you need to send in body string and number. organization_name: ${JSON.stringify(conditions.organization_name)} and number_of_athlethes: ${JSON.stringify(conditions.number_of_athletes)}`);
+        res.send(`Inappropriate parameters - you need to send in body string and number.
+        organization_name: ${JSON.stringify(conditions.organization_name)} and number_of_athlethes: ${JSON.stringify(conditions.number_of_athletes)}`);
+        return;
+    }
+    // if(conditions.number_of_athletes == undefined || conditions.organization_name == undefined){
+    //     console.log("undefined value");
+    //     res.send(`undefined`);
+    //     return;
+    // }
+
+    // if(req.query.organization_name == "" || req.query.number_of_athletes == ""){
+    //     console.log("empty");
+    //     res.send(`empty`);
+    //     return;
+    // }
+
+    if (conditions.number_of_athletes < 0 || conditions.organization_name < 0){
+        console.log(`Inappropriate parameters - you need to send in body string and a POSITIVE number. organization_name: ${JSON.stringify(conditions.organization_name)} and number_of_athlethes: ${JSON.stringify(conditions.number_of_athletes)}`);
+        res.send(`Inappropriate parameters - you need to send in body string and a POSITIVE number.
+        organization_name: ${JSON.stringify(conditions.organization_name)} and number_of_athlethes: ${JSON.stringify(conditions.number_of_athletes)}`);
+        return;
+    }
+
+    // console.log(conditions)
+    ctrl.athlethes_of_organization(conditions,res)
+    console.log("Client handled at /athlethes_of_organization, GET.");
 
 })
 
