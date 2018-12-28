dd09134ed7132656c46d5009252c6597b5a2700e
ShohamRoditi <shohamroditi@gmail.com>
2018-12-28 23:17:12 +0200

    added async await

--- controller.js
+++ controller.js
@@ -29,55 +29,91 @@ mongoose.connect(url, options)
 
 module.exports = {
     //Query number 1 in GET: show all the data - sport associations
-    getAllAssociations:function(res){
-             sportassociation.find({}, (err, associations)=>{
-                if(err) {
-                    console.log (`query error :${err}`)
-                    res.status(404).send('not found');
-                }
-                else res.send(JSON.stringify(associations))
-            })
+    getAllAssociations:async function(res){
+        const result = await sportassociation.find({});
+        if (result) res.json(result);
+        else res.status(404).send('not found Sport Associations');
     },
 
     // Query number 2 in POST: receives two params : year(number) and association_manager(string)
-    updateAssociationByYear:function(conditions,update, res){
-            sportassociation.updateOne(conditions, update, 
-                (err, result) => {
-                    if(err){
-                        console.log(`err: ${err}`);
-                        res.send(err);
-                    }
+    updateAssociationByYear:async function(req, res){
+        const conditions = {year:req.body.year};
+        const update = {association_manager:req.body.association_manager};
 
-                    else if(result.nModified  <= 0){
-                        res.send(`Did not update: ${JSON.stringify(update)} with conditions: ${JSON.stringify(conditions)}`);
-                        console.log(`Did not update: ${JSON.stringify(update)} with conditions: ${JSON.stringify(conditions)}`);
-                    }
+        // this check if the params that sent in body == undefined || == empty 
+        if(!conditions.year || !update.association_manager){
+            console.log(`Inappropriate parameters - you need to send in body number and string.\nyear: ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
+            res.send(`Inappropriate parameters - you need to send in body number and string.
+            year:${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
+            return;
+        }
+    
+        if(conditions.year < 0 || update.association_manager < 0){
+            console.log(`Inappropriate parameters - you need to send in body a POSITIVE number and string.\nyear: ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
+            res.send(`Inappropriate parameters - you need to send in body a POSITIVE number and string.
+            year: ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
+            return;
+        }
+
+        const result = await sportassociation.updateOne(conditions, update);
+        
+        if(!result){
+            res.status(404).send(`No Documents were found with the conditions: ${JSON.stringify(conditions)}.`);
+            return;
+        }
+        
+        else if(result.nModified <= 0){
+            res.send(`Did not update: ${JSON.stringify(update)} with conditions: ${JSON.stringify(conditions)}`);
+            console.log(`Did not update: ${JSON.stringify(update)} with conditions: ${JSON.stringify(conditions)}`);
+        }
                     
-                    else{
-                        console.log(`Successfully Updated SpoartAssocistion with year: ${conditions.year} .Association manager is set to: ${update.association_manager}` );
-                        res.send(`Successfully Updated SpoartAssocistion with year: ${conditions.year} .Association manager is set to: ${update.association_manager}` );
-                    }
-                })
+        else{
+            console.log(`Successfully Updated SpoartAssocistion with year: ${conditions.year} .Association manager is set to: ${update.association_manager}` );
+            res.send(`Successfully Updated SpoartAssocistion with year: ${conditions.year} .Association manager is set to: ${update.association_manager}` );
+            }
 
     },
      
-    // Query number 3 in GET: receives two params : organization_name(string) and number_of_athletes(number)
-    athletes_of_organization:function(conditions, res){
-        sportassociation.find(conditions, (err, association) => {
-            if(err){
-                console.log(`Query Error: ${err}`);
-                res.status(404).send(`not found: ${err}`);
+    // Query number 3 in GET: receives two params : organization_name(string) and money_paid(number)
+    money_of_organization:async function(req, res){
+        const conditions = { organization_name:req.query.organization_name, money_paid:req.query.money_paid};
+    
+        // this check if the params that sent in body == undefined || == empty
+        if(!conditions.money_paid || !conditions.organization_name){
+            console.log(`Inappropriate parameters - you need to send in body string and number.\norganization_name: ${JSON.stringify(conditions.organization_name)} and money_paid: ${JSON.stringify(conditions.money_paid)}`);
+            res.send(`Inappropriate parameters - you need to send in body string and number.
+            organization_name: ${JSON.stringify(conditions.organization_name)} and money_paid: ${JSON.stringify(conditions.money_paid)}`);
+            return;
+        }
+    
+        if (conditions.money_paid < 0 || conditions.organization_name < 0){
+            console.log(`Inappropriate parameters - you need to send in body string and a POSITIVE number.\norganization_name: ${JSON.stringify(conditions.organization_name)} and money_paid: ${JSON.stringify(conditions.money_paid)}`);
+            res.send(`Inappropriate parameters - you need to send in body string and a POSITIVE number.
+            organization_name: ${JSON.stringify(conditions.organization_name)} and money_paid: ${JSON.stringify(conditions.money_paid)}`);
+            return;
+        }
+        //validation for if the money_paid is number and not a string 
+        if(isNaN(conditions.money_paid)){
+            console.log(`You send in params in field money_paid String: ${JSON.stringify(conditions.money_paid)} and not a number - please try again`);
+            res.send(`You send in params in field money_paid String: ${JSON.stringify(conditions.money_paid)} and not a number - please try again`);
+            return;
 
-            }
-            else if(association.length == 0){
-                        res.send(`No documents found: ${JSON.stringify(conditions)}`)
-                        console.log(`No documents found:  ${JSON.stringify(conditions)}`)
-                    }
-            else {
-                res.send(JSON.stringify(association));
-                console.log(`The query with params: ${JSON.stringify(conditions)} is success`)
-            }             
-        })
+        }
+        const result = await sportassociation.find(conditions);
+
+        if(!result){
+            console.log(`not found`);
+            res.status(404).send(`not found`);
+        }
+
+        else if(result.length == 0){
+                res.send(`No documents found: ${JSON.stringify(conditions)}`)
+                console.log(`No documents found:  ${JSON.stringify(conditions)}`)
+        }
+        else {
+            res.send(JSON.stringify(result));
+            console.log(`The query with params: ${JSON.stringify(conditions)} is success`)
+        }             
     }
   
     
--- index.js
+++ index.js
@@ -19,47 +19,13 @@ app.get('/getAllAssociations', (req,res) => {
     
 
 app.post('/updateAssociationByYear', (req, res)=> {
-    const conditions = {year:req.body.year};
-    const update = {association_manager:req.body.association_manager};
-     
-    // this check if the params that sent in body == undefined || == empty 
-    if(!conditions.year || !update.association_manager){
-        console.log(`Inappropriate parameters - you need to send in body number and string.\nyear: ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
-        res.send(`Inappropriate parameters - you need to send in body number and string.
-        year:${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
-        return;
-    }
-
-    if(conditions.year < 0 || update.association_manager < 0){
-        console.log(`Inappropriate parameters - you need to send in body a POSITIVE number and string.\nyear: ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
-        res.send(`Inappropriate parameters - you need to send in body a POSITIVE number and string.
-        year: ${JSON.stringify(conditions.year)} and association_manager: ${JSON.stringify(update.association_manager)}`);
-        return;
-    }
-    ctrl.updateAssociationByYear(conditions,update, res);
+    ctrl.updateAssociationByYear(req, res);
     console.log("Client handled at /updateAssociationByYear, POST.");
 })
 
-app.get('/athletes_of_organization',(req, res) => {
-    const conditions = { organization_name:req.query.organization_name, number_of_athletes:req.query.number_of_athletes};
-    
-    // this check if the params that sent in body == undefined || == empty || == 0 undifiend /null/ empty/!=0
-    if(!conditions.number_of_athletes || !conditions.organization_name){
-        console.log(`Inappropriate parameters - you need to send in body string and number.\norganization_name: ${JSON.stringify(conditions.organization_name)} and number_of_athlethes: ${JSON.stringify(conditions.number_of_athletes)}`);
-        res.send(`Inappropriate parameters - you need to send in body string and number.
-        organization_name: ${JSON.stringify(conditions.organization_name)} and number_of_athlethes: ${JSON.stringify(conditions.number_of_athletes)}`);
-        return;
-    }
- 
-    if (conditions.number_of_athletes < 0 || conditions.organization_name < 0){
-        console.log(`Inappropriate parameters - you need to send in body string and a POSITIVE number.\norganization_name: ${JSON.stringify(conditions.organization_name)} and number_of_athlethes: ${JSON.stringify(conditions.number_of_athletes)}`);
-        res.send(`Inappropriate parameters - you need to send in body string and a POSITIVE number.
-        organization_name: ${JSON.stringify(conditions.organization_name)} and number_of_athlethes: ${JSON.stringify(conditions.number_of_athletes)}`);
-        return;
-    }
-
-    ctrl.athletes_of_organization(conditions,res)
-    console.log("Client handled at /athletes_of_organization, GET.");
+app.get('/money_of_organization',(req, res) => {
+    ctrl.money_of_organization(req,res)
+    console.log("Client handled at /money_of_organization, GET.");
 })
 
 app.get('/api', (req, res) => {
